64758a08d7d9d515a180dcf8d83d4f008a0b9230
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-23 15:48:54 +0530

    :sparkles: Add package

--- cli.ts
+++ cli.ts
@@ -1,8 +1,14 @@
-import { mode } from "./mod.ts";
+import { dpx } from "./mod.ts";
 
 // https://deno.land/manual/tools/script_installer
 if (import.meta.main) {
-  for (let arg of Deno.args) {
-    console.log(arg, mode());
-  }
+  const flags: string[] = [];
+  const args: string[] = [];
+  let packageName = "";
+  Deno.args.forEach((arg, index) => {
+    if (index === 0) packageName = arg;
+    else if (arg.startsWith("--")) flags.push(arg);
+    else args.push(arg);
+  });
+  dpx(packageName, flags, args);
 }
--- mod.ts
+++ mod.ts
@@ -1,4 +1,33 @@
-/** JSDoc for this line */
-export function mode() {
-  return 0;
+/** Get the file to run */
+export async function getEntryFile(packageName: string) {
+  const REPO_URL = `https://deno.land/x/${packageName}`;
+  try {
+    const cliFile = await (await fetch(`${REPO_URL}/cli.tx`)).text();
+    if (cliFile) return `${REPO_URL}/cli.tx`;
+  } catch (error) {}
+  try {
+    const modFile = await (await fetch(`${REPO_URL}/mod.ts`)).text();
+    if (modFile) return `${REPO_URL}/mod.ts`;
+  } catch (error) {}
+  throw new Error("Could not find entry file");
+}
+
+/**
+ * Run a Deno package
+ * @param packageName - Package name
+ * @param flags - Deno flags, like --allow-net
+ * @param args - CLI args
+ */
+export async function dpx(
+  packageName: string,
+  flags: string[],
+  args: string[]
+) {
+  const filePath = await getEntryFile(packageName);
+  return Deno.run({
+    cmd: ["deno", "run", ...flags, filePath, ...args],
+    stdout: "inherit",
+    stderr: "inherit",
+    stdin: "inherit",
+  }).status();
 }
