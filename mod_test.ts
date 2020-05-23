e037e783160130d6af062f5ef2f3bef156bd2f89
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-23 16:06:26 +0530

    :white_check_mark: Add tests

--- mod_test.ts
+++ mod_test.ts
@@ -1,6 +1,22 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { mode } from "./mod.ts";
+import { getEntryFile } from "./mod.ts";
 
-Deno.test("test starter function", async (): Promise<void> => {
-  assertEquals(mode(), 0);
-});
+Deno.test(
+  "get entry file - cli.ts",
+  async (): Promise<void> => {
+    assertEquals(
+      await getEntryFile("online"),
+      "https://deno.land/x/online/cli.ts"
+    );
+  }
+);
+
+Deno.test(
+  "get entry file - mod.ts",
+  async (): Promise<void> => {
+    assertEquals(
+      await getEntryFile("recursive_readdir"),
+      "https://deno.land/x/recursive_readdir/mod.ts"
+    );
+  }
+);
