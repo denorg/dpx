fc9b9bf5cc08b016730d8ba95e89c3684a1cc9eb
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-28 14:05:32 +0530

    Allow all Deno flags (fixed #2)

--- cli.ts
+++ cli.ts
@@ -1,5 +1,25 @@
 import { dpx } from "./mod.ts";
 
+const DENO_FLAGS = [
+  "-A",
+  "--allow-all",
+  "--allow-env",
+  "--allow-hrtime",
+  "--allow-net=",
+  "--allow-plugin",
+  "--allow-read=",
+  "--allow-run",
+  "--allow-write=",
+  "--reload",
+  "-R",
+  "--lock=",
+  "--importmap=",
+  "--unstable",
+  "--inspect",
+  "--inspect-brk",
+  "--root",
+];
+
 // https://deno.land/manual/tools/script_installer
 if (import.meta.main) {
   const flags: string[] = [];
@@ -7,7 +27,15 @@ if (import.meta.main) {
   let packageName = "";
   Deno.args.forEach((arg, index) => {
     if (index === 0) packageName = arg;
-    else if (arg.startsWith("--")) flags.push(arg);
+    let isDenoFlag = false;
+    DENO_FLAGS.forEach((flag) => {
+      if (
+        flag === arg ||
+        (flag.endsWith("=") && arg.startsWith(flag.slice(0, -1)))
+      )
+        isDenoFlag = true;
+    });
+    if (isDenoFlag) flags.push(arg);
     else args.push(arg);
   });
   dpx(packageName, flags, args);
